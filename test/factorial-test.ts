8bcae12f673e8a12527d569632f356fa1a140e40
David Valles <davidtjvalles@gmail.com>
2020-09-22 05:56:49 -0700

    Add factorial solution and test

---
+++ test/factorial-test.ts
@@ -0,0 +1,15 @@
+import { test } from 'tap'
+import { factorial } from '../src/factorial'
+
+test('factorial: returns the factorial of N', (t) => {
+  const numbers = [0, 1, 2, 3, 4]
+  const results = numbers.map(factorial)
+  const expected = [1, 1, 2, 6, 24]
+  t.plan(1)
+  t.deepEqual(results, expected)
+})
+
+test('factorial: throws RangeError on negative input', (t) => {
+  t.plan(1)
+  t.throws(() => factorial(-5))
+})
